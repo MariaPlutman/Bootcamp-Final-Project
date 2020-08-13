9e0827703fcbb8007620abc04a95f1eaaf594ac2
MariaPlutman <mariaplutman@gamil.com>
2020-08-13 18:09:11 +0300

    request form

---
+++ crm_app/project/auth_blueprint.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required
+from .models import User
+from . import db
+from .forms import RequestForm
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return render_template('login.html')
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login'))
+
+    login_user(user, remember=remember)
+    return redirect(url_for('main.profile'))
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login.html'))
+
+@auth.route('/inquiries',methods=('GET', 'POST'))
+def request():
+	form = RequestForm()
+	if form.validate_on_submit():
+		return redirect(url_for('success'))
+    return render_template('login.html',form=form)
+
+
+
+
