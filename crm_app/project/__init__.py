2d544e912dba91b78d418c22a024d6273cb93c73
MariaPlutman <mariaplutman@gamil.com>
2020-08-18 19:53:53 +0300

    correct the profile

--- crm_app/project/__init__.py
+++ crm_app/project/__init__.py
@@ -3,17 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-login_manager = LoginManager()
-app = Flask(__name__)
 
 
 def create_app():
+    app = Flask(__name__)
 
     app.config['SECRET_KEY'] = 'secret-key-goes-here'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
     db.init_app(app)
 
+    login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
--- crm_app/project/main_blueprint.py
+++ crm_app/project/main_blueprint.py
@@ -9,7 +9,7 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('login.html')
+    return render_template('index.html')
 
 
 @main.route('/profile')
@@ -18,16 +18,21 @@ def profile():
     return render_template('profile.html', name=current_user.name)
 
 
-@main.route('/request/new', methods=['POST'])
-def submit_request():
-    form = RequestForm()
+# @main.route('/request', methods=['POST'])
+# def submit_request():
+#     form = RequestForm()
 
-    message = "יש לך פנייה חדשה"
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login("skillzSupport@taldor.co.il", "PASSWORD")
+#     message = "יש לך פנייה חדשה"
+#     server = smtplib.SMTP("smtp.gmail.com", 587)
+#     server.starttls()
+#     server.login("skillzSupport@taldor.co.il", "PASSWORD")
 
-    if form.validate_on_submit():
-        server.sendmail("", message)
-        return redirect(url_for('success'))
-    return render_template('request.html', form=form)
+#     if form.validate_on_submit():
+#         server.sendmail("", message)
+#         return redirect(url_for('success'))
+#     return render_template('request.html', form=form)
+
+
+@main.route('/request')
+def inquiries():
+    return render_template('request.html')
